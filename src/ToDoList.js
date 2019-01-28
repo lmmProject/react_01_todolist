ff6468d7d972733aaa3db5a9f1c921aedc69446f
lmm <752634866@qq.com>
2019-01-28 17:11:05 +0800

    component

---
+++ src/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ToDoList extends React.Component{
+
+    render(){
+        return (
+            //  jsx 语法
+            <div>
+                hello, i am jsx
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
