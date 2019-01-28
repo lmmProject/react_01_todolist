b68370613fed2e658aabf256b3f5417b9d8198db
lmm <752634866@qq.com>
2019-01-28 15:42:41 +0800

    cs提交

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        删减之后的项目结构
+      </div>
+    );
+  }
+}
+
+export default App;
