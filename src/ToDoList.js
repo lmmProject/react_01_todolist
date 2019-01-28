bcbd22b21b278b8c599e9148218c840aecb18394
lmm <752634866@qq.com>
2019-01-28 19:30:33 +0800

    bind

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,12 +2,39 @@ import React from 'react'
 
 class ToDoList extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            list : [
+                'learn english',
+                'learn jsx',
+                'learn react'
+            ]
+        }
+    }
+
+    handleBtnClick() {
+        this.setState({
+            list: [...this.state.list, 'Hello World']
+        })
+    }
+
     render(){
         return (
             //  jsx 语法
-            <div>
-                hello, i am jsx
-            </div>
+            <React.Fragment>
+                 <div>
+                    <input />
+                    <button onClick={this.handleBtnClick.bind(this)}>add</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })
+                    }
+                </ul>
+            </React.Fragment>
         );
     }
 }
